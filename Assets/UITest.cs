5e64094853e81f2a1956af252c7f6add057a6868
Mike <mhilts7643@conestogac.on.ca>
2021-03-10 22:42:28 -0500

    Testing showing ui on remote clients

---
+++ Assets/UITest.cs
@@ -0,0 +1,40 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UserInstantiation;
+
+public class UITest : MonoBehaviourPunCallbacks
+{
+    private PhotonView view;
+
+    public GameObject button;
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ((UserType)PhotonNetwork.LocalPlayer.CustomProperties["Type"] == UserType.Performer)
+        {
+            panel.SetActive(false);
+        }
+        else
+        {
+            panel.SetActive(false);
+            button.SetActive(false);
+        }
+
+        view = gameObject.GetComponent<PhotonView>();
+    }
+
+    private void ButtonClick()
+    {
+        view.RPC("DisplayPanel", RpcTarget.Others);
+    }
+
+    [PunRPC]
+    public void DisplayPanel()
+    {
+        panel.SetActive(true);
+    }
+}
