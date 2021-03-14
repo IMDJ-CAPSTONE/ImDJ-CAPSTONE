07d720d62539f69fc90088fd511cbe11aa2a2fe9
ivan granic <igranic@conestogac.on.ca>
2021-03-13 22:18:23 -0400

    added lean ui, started work on the voting system in ui

---
+++ Assets/Scripts/UI/UIManagement.cs
@@ -0,0 +1,52 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UserInstantiation;
+
+public class UIManagement : MonoBehaviourPunCallbacks
+{
+    private PhotonView view;
+
+    public GameObject DesktopUIResource;
+    
+    public GameObject PerformerUIResource;
+
+    private Dictionary<string, GameObject> UIContainers = new Dictionary<string, GameObject>();
+
+
+    public GameObject button;
+    public GameObject twitchPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ((UserType)PhotonNetwork.LocalPlayer.CustomProperties["Type"] == UserType.Performer)
+        {
+            UIContainers.Add("PerformerUI", Instantiate(PerformerUIResource));
+            UIContainers["PerformerUI"].transform.SetParent(gameObject.transform);
+            button = UIContainers["PerformerUI"].GetComponent<PerformerUserUIController>().button;
+        }
+        else if ((UserType)PhotonNetwork.LocalPlayer.CustomProperties["Type"] == UserType.Desktop)
+        {
+            UIContainers.Add("DesktopUI", Instantiate(DesktopUIResource));
+            UIContainers["DesktopUI"].transform.SetParent(gameObject.transform);
+            twitchPanel = UIContainers["DesktopUI"];
+            DisplayPanel();
+        }
+
+        view = gameObject.GetComponent<PhotonView>();
+    }
+
+    public void ButtonClick()
+    {
+        view.RPC("DisplayPanel", RpcTarget.Others);
+    }
+
+    [PunRPC]
+    private void DisplayPanel()
+    {
+        twitchPanel.SetActive(true);
+    }
+}
