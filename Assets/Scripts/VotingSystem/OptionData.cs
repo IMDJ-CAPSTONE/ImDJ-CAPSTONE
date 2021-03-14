4581b1493f551be9bd9ccefbbb5999a107c1b524
Divyang <divyangdankhara007@outlook.com>
2021-03-14 18:54:29 -0400

    Added Twitch chat support to voting system.

---
+++ Assets/Scripts/VotingSystem/OptionData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class OptionData : MonoBehaviour
+{
+
+	public string OptionName;
+	public int VoteCount;
+
+	public OptionData()
+	{
+		VoteCount = 0;
+	}
+
+	public OptionData(string optionName, int voteCount = 0)
+	{
+		this.OptionName = optionName;
+		this.VoteCount = voteCount;
+	}
+}
--- Assets/Scripts/VotingSystem/VotingSystemController.cs
+++ Assets/Scripts/VotingSystem/VotingSystemController.cs
@@ -1,19 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TwitchLib.Client.Models;
+using TwitchLib.Unity;
 using UnityEngine;
 
 
 public class VotingSystemController : MonoBehaviour
 {
+    #region Public Members
+    public string Question = "";
+	public string TwitchChannelName = "";
+    #endregion
 
-	public string Question;
-	public Dictionary<int, optionData> options = new Dictionary<int, optionData>();
-	
+
+    #region Private Members
+    private Client _client;
+	private Dictionary<int, OptionData> options = new Dictionary<int, OptionData>();
+    #endregion
+
+    private void Start() 
+	{
+		Application.runInBackground = true;
+
+		//Create Credentials instance
+		ConnectionCredentials credentials = new ConnectionCredentials(TwitchChannelName, "AuthToken");
+
+		// Create new instance of Chat Client
+		_client = new Client();
+
+		// Initialize the client with the credentials instance, and setting a default channel to connect to.
+		_client.Initialize(credentials, TwitchChannelName);
+
+		// Bind callbacks to events
+		//_client.OnConnected += OnConnected;
+		//_client.OnJoinedChannel += OnJoinedChannel;
+		//_client.OnMessageReceived += OnMessageReceived;
+		_client.OnChatCommandReceived += OnChatCommandReceived;     //research this
+																	//_client.OnWhisperReceived += OnWhisperReceived;
+																	//_client.OnNewSubscriber += OnNewSubscriber;
+																	//_client.OnBeingHosted += OnBeingHosted;
+																	//_client.OnGiftedSubscription += OnGiftedSub;
+																	//_client.OnModeratorJoined += OnModJoin;
+																	//_client.OnReSubscriber += OnReSub;
+																	//_client.OnUserBanned += UserBanned;
+
+
+		// Connect
+		_client.Connect();
+	}
+
 	public void AddOption(string optionName) 
 	{
 		//adding option to options list to keep track of voting count
 		
-		var option = new optionData(optionName);
+		var option = new OptionData(optionName);
 		options.Add(options.Count + 1, option);	
 	}
 
@@ -24,7 +64,10 @@ public class VotingSystemController : MonoBehaviour
 
 	public void Voting(int optionNumber) 
 	{
-		options[optionNumber].VoteCount += 1;
+		if(optionNumber != 0 && options != null)
+        {
+			options[optionNumber].VoteCount += 1;
+        }
     }
 
 	public int GetVoteCount(int optionNumber) 
@@ -37,18 +80,49 @@ public class VotingSystemController : MonoBehaviour
 		Question = "";
     }
 
-}
-public class optionData {
-	
-	public string OptionName;
-	public int VoteCount;
+	private void OnChatCommandReceived(object sender, TwitchLib.Client.Events.OnChatCommandReceivedArgs e)
+	{
+		Debug.Log($"Command received from {e.Command.ChatMessage.DisplayName}: {e.Command.ChatMessage.Message}");
 
-    public optionData() {
-		VoteCount = 0;
-    }
+		int selectedOption = 0;
 
-    public optionData(string optionName, int voteCount = 0) {
-		this.OptionName = optionName;
-		this.VoteCount = voteCount;
-    }
+		switch (e.Command.CommandText)
+        {
+			case "vote1":
+				selectedOption = 1;
+				break;
+
+			case "vote2":
+				selectedOption = 2;
+				break;
+
+			case "vote3":
+				selectedOption = 3;
+				break;
+
+			case "vote4":
+				selectedOption = 4;
+				break;
+			default:
+				selectedOption = 0;
+				break;
+		};
+		
+
+		Voting(selectedOption);
+	}
+
+	private void SendPollToChat()
+    {
+		string message = "New Poll";
+
+		foreach (KeyValuePair<int, OptionData> entry in options)
+		{
+			message += '\n';
+			message = $"{message} {entry.Key} : {entry.Value}";
+		}
+
+		_client.SendMessage(TwitchChannelName, message);
+	}
+
 }
