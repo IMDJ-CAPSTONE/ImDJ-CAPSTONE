3e894d96da395d4899e808451c6ebd7034848041
ivan granic <igranic@conestogac.on.ca>
2021-03-21 20:58:35 -0400

    voting ui fix

--- Assets/Scripts/UI/DesktopUserVotingUI.cs
+++ Assets/Scripts/UI/DesktopUserVotingUI.cs
@@ -6,16 +6,27 @@ using UnityEngine;
 
 public class DesktopUserVotingUI : MonoBehaviour
 {
-    public GameObject[] voteButtons;
+    public GameObject voteButtonPrefab;
+    private GameObject[] voteButtons;
+    public GameObject votingMenu;
 
     public Action<int> voteOption;
+
+    public int optionCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        voteButtons[0].GetComponent<LeanButton>().OnClick.AddListener(() => { sendVote(1); });
-        voteButtons[1].GetComponent<LeanButton>().OnClick.AddListener(() => { sendVote(2); });
-        voteButtons[2].GetComponent<LeanButton>().OnClick.AddListener(() => { sendVote(3); });
-        voteButtons[3].GetComponent<LeanButton>().OnClick.AddListener(() => { sendVote(4); });
+        voteButtons = new GameObject[optionCount];
+
+        for (int i = 0; i < optionCount; i++)
+        {
+            int copy = i + 1;
+            voteButtons[i] = Instantiate(voteButtonPrefab);
+            voteButtons[i].name = "Vote Option: " + i.ToString(); 
+            voteButtons[i].transform.SetParent(votingMenu.transform);//need to change parent
+            voteButtons[i].GetComponent<LeanButton>().OnClick.AddListener(() => { sendVote(copy); });
+        }
     }
 
     // Update is called once per frame
