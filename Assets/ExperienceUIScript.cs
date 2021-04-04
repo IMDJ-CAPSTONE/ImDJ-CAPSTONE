a1ddf17d9974634e759a26dbe14b59a4bc2d30f2
Jason k <jason@kassies.ca>
2021-04-03 23:00:11 -0400

    adding exit and fullscreen buttons

---
+++ Assets/ExperienceUIScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperienceUIScript : MonoBehaviour
+{
+    public GameObject GameMenu;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    public void fullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+}
