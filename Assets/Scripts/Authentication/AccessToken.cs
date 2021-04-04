35a362a7c0334c4868bfb7326680044038ac2372
Divyang <divyangdankhara007@outlook.com>
2021-04-04 19:11:29 -0400

    [IMDJ-40] - Added auth with Twitch

---
+++ Assets/Scripts/Authentication/AccessToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class AccessToken
+{
+    public string access_token { get; set; }
+    public int expires_in { get; set; } = 0;
+    public string id_token { get; set; }
+    public string refresh_token { get; set; }
+    public string[] scope { get; set; }
+    public string token_type { get; set; }
+}
