f26cc02319181eb36d4357d26658b7fb0d879764
Jason k <jason@kassies.ca>
2021-04-11 14:24:00 -0400

    tweaked resolutions dropdown for better UX
    increased scroll speed and fixed a bug that was no longer present but left code in comments

--- Assets/ExperienceUIScript.cs
+++ Assets/ExperienceUIScript.cs
@@ -25,21 +25,20 @@ public class ExperienceUIScript : MonoBehaviour
         
         for (int i = 0; i < resolutions.Length; i++)
         {
-            dropdownMenu.options[i].text = ResToString(resolutions[i]);
+            //there was an issue with this creating multiples of some resolutions but the bug dissapeared
+            //either way this should prevent it, if it crops up again
+            ///if(!dropdownMenu.options.Contains(new Dropdown.OptionData(dropdownMenu.options[i].text)))
+            dropdownMenu.options[i].text = resolutions[i].width + " x " + resolutions[i].height;
             dropdownMenu.value = i;
             dropdownMenu.options.Add(new Dropdown.OptionData(dropdownMenu.options[i].text));
-
+            
         }
 
         //check if the payler has old volume setting and set it
         mixer.SetFloat("MasterVol", PlayerPrefs.GetFloat("MasterVol"));
     }
 
-    string ResToString(Resolution res)
-    {
-        return res.width + " x " + res.height;
-    }
-
+    
     void Update()
     {
         
