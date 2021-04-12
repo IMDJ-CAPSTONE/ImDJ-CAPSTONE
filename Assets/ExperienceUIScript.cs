28a878644ccc834630db64602389295a0c85b8fd
Jason k <jason@kassies.ca>
2021-04-12 09:03:27 -0400

    this is the final form
    if any unexpected results happen, its a feature

--- Assets/ExperienceUIScript.cs
+++ Assets/ExperienceUIScript.cs
@@ -23,20 +23,19 @@ public class ExperienceUIScript : MonoBehaviour
         resolutions = Screen.resolutions;
         dropdownMenu.onValueChanged.AddListener(delegate 
             { Screen.SetResolution(resolutions[dropdownMenu.value].width, 
-                                   resolutions[dropdownMenu.value].height, false, 60); });
+                                   resolutions[dropdownMenu.value].height, false, 50); });
         
         
         for (int i = 0; i < resolutions.Length; i++)
         {
             //there was an issue with this creating multiples of some resolutions but the bug dissapeared
             //either way this should prevent it, if it crops up again
-            //if (!dropdownMenu.options.Contains(new Dropdown.OptionData(dropdownMenu.options[i].text)))
-            //{
-                dropdownMenu.options[i].text = resolutions[i].width + " x " + resolutions[i].height + "  " + resolutions[i].refreshRate;
+            if (dropdownMenu.options.Contains(new Dropdown.OptionData(dropdownMenu.options[i].text)) == false)
+            {
+                dropdownMenu.options[i].text = resolutions[i].width + " x " + resolutions[i].height;
                 dropdownMenu.value = i;
                 dropdownMenu.options.Add(new Dropdown.OptionData(dropdownMenu.options[i].text));
-            //}
-            
+            }
         }
 
         //check if the payler has old volume setting and set it
