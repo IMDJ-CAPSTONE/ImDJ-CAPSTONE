ab3a7f1e67205fe183af3e5e0640f2e1bc88e203
Jason k <jason@kassies.ca>
2021-04-22 12:27:57 -0400

    commenting VotingSystem scripts

--- Assets/Scripts/VotingSystem/OptionData.cs
+++ Assets/Scripts/VotingSystem/OptionData.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿/*  FILE          : 	OPtionData.cs
+*   PROJECT       : 	PROG3221 - Capstone
+*   PROGRAMMER    : 	Ivan Granic, Jason Kassies, Div Dankahara, Mike Hilts
+*   FIRST VERSION : 	2021-04-05
+*   DESCRIPTION   : 	Contains the class used to store the text and how many votes cast for each option in a Poll
+*/
+using UnityEngine;
 
 public class OptionData : MonoBehaviour
 {
